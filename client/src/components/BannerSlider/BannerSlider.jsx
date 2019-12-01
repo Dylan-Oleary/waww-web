4c3dc23a7ac1a852b9e0978f130987e7331313ef
Dylan-Oleary <dylaneamusic@gmail.com>
2019-12-01 01:10:20 -0500

    Added BannerSlider component and index files missing from last commit

---
+++ client/src/components/BannerSlider/BannerSlider.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Slider from "react-slick";
+import { Link } from "react-router-dom";
+
+import altLogo from "../../public/assets/images/WAWW-pink.png";
+
+const sliderSettings = {
+    arrows: false,
+    autoplay: true,
+    className: 'banner-slider',
+    dots: false,
+    pauseOnHover: true,
+};
+
+const BannerSlider = ({ items, className }) => {
+    const onError = event => {
+        event.target.src = altLogo;
+    };
+
+    return (
+        <div id="BannerSlider">
+            <Slider {...sliderSettings}>
+                {items.map(item => (
+                    <div
+                        className={className}
+                        key={`banner-slider-${item._id}`}
+                    >
+                        <img
+                            src={item.backdropPath}
+                            alt={item.label}
+                            onError={onError}
+                        />
+                        <Link
+                            className="title-overlay"
+                            to={item.path}
+                        >
+                            {item.label}
+                        </Link>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+};
+
+export default BannerSlider;
